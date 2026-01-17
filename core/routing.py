eb6c55183f040904cf8d9e487bc49bb38813a079
Gregory Grant <grant8827@yahoo.com>
2026-01-17 17:54:41 -0500

    Fix core routing to use VideoCallConsumer instead of VideoSessionConsumer

--- core/routing.py
+++ core/routing.py
@@ -3,9 +3,9 @@
 WebSocket routing configuration for TheraCare.
 """
 
-from django.urls import path
-from telehealth.consumers import VideoSessionConsumer
+from django.urls import re_path
+from telehealth.consumers import VideoCallConsumer
 
 websocket_urlpatterns = [
-    path('ws/video/<str:room_id>/', VideoSessionConsumer.as_asgi()),
+    re_path(r'ws/video/(?P<session_id>\w+)/$', VideoCallConsumer.as_asgi()),
 ]
